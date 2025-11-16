7b89025be87c656de08a07002deaa5faf8054f77
EnzEdu <enzinhovlogs@outlook.com>
2025-11-16 18:20:16 -0300

    feat: Suporte na database para conversas e mensagens

--- inforbot.py
+++ inforbot.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template, request, session, redirect, url_for, flash
-from models import db, AppUser
+from models import db, M_AppUser, M_Conversa, M_Mensagem
 from config import Config
 from flask_migrate import Migrate
+from typing import List
 
 def app():
     app = Flask(__name__)
@@ -21,7 +22,7 @@ def app():
             usuario = request.form["usuario"]
             senha = request.form["senha"]
 
-            user : AppUser = AppUser.query.filter_by(usuario=usuario).first()
+            user : M_AppUser = M_AppUser.query.filter_by(usuario=usuario).first()
             if user and user.checa_senha(senha):
                 session["user_id"] = user.id
                 session["usuario"] = user.usuario
@@ -30,10 +31,6 @@ def app():
                 flash("Credenciais invalidas.")
                 return redirect(url_for("login"))
         return render_template("login.html")
-
-    @app.route("/dashboard")
-    def dashboard():
-        return render_template("dashboard.html")
 
     @app.route("/register", methods=["GET", "POST"])
     def register():
@@ -49,19 +46,63 @@ def app():
                 #return redirect(url_for("register"))
 
             # Check de campos em uso
-            if AppUser.query.filter((AppUser.usuario == usuario) | (AppUser.email == email)).first():
+            if M_AppUser.query.filter((M_AppUser.usuario == usuario) | (M_AppUser.email == email)).first():
                 flash("Nome de usuario ou email em uso.")
                 return redirect(url_for("register"))
 
-            user = AppUser(nomeCompleto=nomeCompleto, usuario=usuario, email=email)
+            user = M_AppUser(nomeCompleto=nomeCompleto, usuario=usuario, email=email)
             user.set_senha_hasheada(senha)
 
+            # PLACEHOLDER
+            conversa_inicial = M_Conversa(t_appuser=user, titulo="Conversa inicial")
+
             db.session.add(user)
+            db.session.add(conversa_inicial)
             db.session.commit()
 
             return redirect(url_for("login"))
         return render_template("register.html")
-    
+
+
+    @app.route("/dashboard", methods=["GET", "POST"])
+    def dashboard():
+        # PLACEHOLDER
+        if request.method == "GET":
+            print(session["usuario"])
+            conversaInicial : M_Conversa = M_Conversa.query.filter(
+                (M_Conversa.appuser_id == session["user_id"]) & (M_Conversa.titulo == "Conversa inicial")
+            ).first()
+            mensagens : List[M_Mensagem]= M_Mensagem.query.filter((M_Mensagem.conversa_id == conversaInicial.id)).all()
+
+            lista_mensagens = [(["USUARIO" if mensagem.autor == session["usuario"] else "BOT"], mensagem.texto) for mensagem in mensagens]
+            print(lista_mensagens)
+
+            return render_template("dashboard.html", lista_mensagens=lista_mensagens)
+
+        elif request.method == "POST":
+            usuarioAppUser : M_AppUser = M_AppUser.query.filter((M_AppUser.id == session["user_id"])).first()
+            conversaInicial : M_Conversa = M_Conversa.query.filter(
+                (M_Conversa.appuser_id == session["user_id"]) & (M_Conversa.titulo == "Conversa inicial")
+            ).first()
+
+            msg = M_Mensagem(
+                conversa_id=conversaInicial.id,
+                autor=session["usuario"],
+                text=request.form["user_message"]
+            )
+            db.session.add(msg)
+            db.session.commit()
+        return render_template("dashboard.html")
+
+
+
+
+
+
+
+
+
+
     with app.app_context():
         db.create_all()
 
--- models.py
+++ models.py
@@ -4,22 +4,70 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-class AppUser(db.Model):
-    __tablename__ = "appuser"
+class M_AppUser(db.Model):
+    __tablename__ = "t_appuser"
 
     id              = db.Column(db.Integer, primary_key=True)
     criado_em       = db.Column(db.DateTime, default=datetime.now)
     nomeCompleto    = db.Column(db.String(400), unique=True, nullable=False)
     usuario         = db.Column(db.String(80), unique=True, nullable=False)
     email           = db.Column(db.String(200), unique=True, nullable=False)
-    password_hash   = db.Column(db.String(128), nullable=False)
+    senha_hash      = db.Column(db.String(128), nullable=False)
+
+    # Usuario pode ter n conversas
+    conversas = db.relationship(
+        "M_Conversa",
+        back_populates="t_appuser",
+        cascade="all, delete-orphan",
+        order_by="M_Conversa.criado_em"
+    )
 
     def __repr__(self):
-        return f"<AppUser {self.username}>"
+        return f"<M_AppUser {self.usuario}>"
 
-    def set_senha_hasheada(self, password: str):
-        self.password_hash = generate_password_hash(password)
+    def set_senha_hasheada(self, senha: str):
+        self.senha_hash = generate_password_hash(senha)
 
     # Checa senha inserida com a senha da entrada do bd
-    def checa_senha(self, password: str) -> bool:
-        return check_password_hash(self.password_hash, password)
+    def checa_senha(self, senha: str) -> bool:
+        return check_password_hash(self.senha_hash, senha)
+
+
+
+
+class M_Conversa(db.Model):
+    __tablename__ = "t_conversa"
+
+    appuser_id      = db.Column(db.Integer, db.ForeignKey("t_appuser.id"), nullable=False)
+    id              = db.Column(db.Integer, primary_key=True)
+    criado_em       = db.Column(db.DateTime, default=datetime.now)
+    titulo          = db.Column(db.String(100), unique=True, nullable=False)
+
+    # Conversa esta ligada a 1 usuario
+    t_appuser = db.relationship(
+        "M_AppUser", 
+        back_populates="conversas"
+    )
+
+    # Conversa pode ter n mensagens
+    mensagens = db.relationship(
+        "M_Mensagem",
+        back_populates="t_conversa",
+        cascade="all, delete-orphan",
+        order_by="M_Mensagem.enviada_em"
+    )
+
+
+
+
+class M_Mensagem(db.Model):
+    __tablename__ = "t_mensagem"
+
+    conversa_id     = db.Column(db.Integer, db.ForeignKey("t_conversa.id"), nullable=False) # chave estrangeira
+    id              = db.Column(db.Integer, primary_key=True)
+    enviada_em      = db.Column(db.DateTime, default=datetime.now)
+    autor           = db.Column(db.String(80), unique=True, nullable=False)
+    texto           = db.Column(db.Text, unique=True, nullable=False)
+
+    # Mensagem esta ligada a 1 conversa
+    t_conversa = db.relationship("M_Conversa", back_populates="mensagens")
